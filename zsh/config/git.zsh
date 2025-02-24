ade6c083bda1fd65a77491f7bdc23db95d9b169f
Léo Guillon <82669317+LeoGuillon@users.noreply.github.com>
2025-02-24 11:46:30 +0100

    pro - added 2 new apple music scripts and some git aliases

---
+++ zsh/config/git.zsh
@@ -0,0 +1,14 @@
+
+# ------------------------------------------------------------------------------
+# (ALIASES)
+# ------------------------------------------------------------------------------
+
+alias gpu='git pull'
+alias gs='git status'
+alias gdiff='git diff'
+alias ga='git add'
+alias gaa='git add --all'
+alias gc='git commit -m'
+alias gca='git commit -a -m'
+alias gp='git push'
+alias gco='git checkout'
