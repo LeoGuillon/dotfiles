d23b24c25c8641715cdf8420560c670edcd4b7df
LeoGuillon <82669317+LeoGuillon@users.noreply.github.com>
2025-03-02 19:47:45 +0100

    perso - added gitsigns

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -2,7 +2,11 @@ return {
   "nvim-tree/nvim-tree.lua",
   dependencies = "nvim-tree/nvim-web-devicons",
   config = function()
-    require("nvim-tree").setup({})
+    require("nvim-tree").setup({
+      view = {
+        side = "right",
+      }
+    })
     
     -- (CUSTOM ACTIONS)
     local api = require("nvim-tree.api")
