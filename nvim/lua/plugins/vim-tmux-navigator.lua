e3d85ad7bd49bf425306c3c45ed7531b66880ce6
Léo Guillon <82669317+LeoGuillon@users.noreply.github.com>
2025-07-04 16:39:30 +0200

    pro - fix with a vim-tmux navigator bug

--- nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/lua/plugins/vim-tmux-navigator.lua
@@ -7,9 +7,12 @@ return {
     "TmuxNavigateRight",
   },
   keys = {
-    { "<C-Left>", "<cmd>TmuxNavigateLeft<cr>", { desc = "Go to Left Window", silent = true, remap = true } },
-    { "<C-Down>", "<cmd>TmuxNavigateDown<cr>", { desc = "Go to Down Window", silent = true, remap = true } },
-    { "<C-Up>", "<cmd>TmuxNavigateUp<cr>", { desc = "Go to Up Window", silent = true, remap = true } },
-    { "<C-Right>", "<cmd>TmuxNavigateRight<cr>", { desc = "Go to Right Window", silent = true, remap = true } },
+    { "<C-Left>", "<cmd>TmuxNavigateLeft<cr>", { desc = "Go to Left Window", silent = true, noremap = true } },
+    { "<C-Down>", "<cmd>TmuxNavigateDown<cr>", { desc = "Go to Down Window", silent = true, noremap = true } },
+    { "<C-Up>", "<cmd>TmuxNavigateUp<cr>", { desc = "Go to Up Window", silent = true, noremap = true } },
+    { "<C-Right>", "<cmd>TmuxNavigateRight<cr>", { desc = "Go to Right Window", silent = true, noremap = true } },
   },
+  init = function()
+    vim.g.tmux_navigator_no_mappings = 1 -- to prevent to collision of <C-h> with my mapping of splitting
+  end,
 }
