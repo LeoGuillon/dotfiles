3289f8068a5361198f4b1d3ac4ce8da1675a388b
LeoGuillon <82669317+LeoGuillon@users.noreply.github.com>
2026-09-23 10:16:29 +0200

    moar zsh music commands

--- zsh/config/music.zsh
+++ zsh/config/music.zsh
@@ -5,3 +5,19 @@ deep () {
 Deep () {
   osascript $DOTFILES/applescripts/work_session_with_start.applescript
 }
+
+deepl () {
+  osascript $DOTFILES/applescripts/work_session-long.applescript
+}
+
+Deepl () {
+  osascript $DOTFILES/applescripts/work_session-long_with_start.applescript
+}
+
+deeps () {
+  osascript $DOTFILES/applescripts/work_session-short.applescript
+}
+
+Deeps () {
+  osascript $DOTFILES/applescripts/work_session-short_with_start.applescript
+}
